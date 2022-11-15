7fdc172f1dcf22faaef4444e82da2f29a6210a0e
kipngetich <kipngetich@finplusgroup.com>
2022-11-15 22:58:48 +0300

    Add Feeds App and Settings

---
+++ feeds/feeds/doctype/feeds_settings/feeds_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, 254 ERP and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Feeds Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
