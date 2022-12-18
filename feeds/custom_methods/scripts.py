c8d0f5f5aa7aec1659b495a898fbb36a5ec09a68
kipngetich <kipngetich@finplusgroup.com>
2022-12-18 19:38:24 +0300

    update functionality to update product bundle with the linked customer in item

--- feeds/custom_methods/scripts.py
+++ feeds/custom_methods/scripts.py
@@ -5,7 +5,14 @@ def add_customer_to_product_bundle():
     Function that loops through all the product bundles and updates them using 
     customer in the linked item
     '''
-    print("Linking customer to invoices")
-    list_of_items = frappe.get_list("Item")
-    print(list_of_items)
+    list_of_items = frappe.get_list("Item",fields = ['name','linked_customer'])
+    for item in list_of_items:
+        try:
+            bundle_doc = frappe.get_doc("Product Bundle",item.get('name'))
+            bundle_doc.linked_customer = item.get('linked_customer')
+            bundle_doc.save()
+            frappe.db.commit()
+        except:
+            pass
+                
 
