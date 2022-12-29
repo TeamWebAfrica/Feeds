408ad1e27e4a1d52769c348c997e1586003b8b3a
kipngetich <kipngetich@finplusgroup.com>
2022-12-29 18:11:01 +0300

    Filter modes of payment in Payment Enter per user

---
+++ feeds/custom_methods/payment_entry.py
@@ -0,0 +1,20 @@
+import frappe
+
+@frappe.whitelist(allow_guest=True)
+def filter_payment_modes(doctype, txt, searchfield, start, page_len, filters):
+    '''
+    Function that filters the different modes of payments for the curent user
+    '''
+    filters['user'] = "khalifngeno@gmail.com"
+
+    custom_filters = {}
+    if filters.get('user') != 'Administrator':
+        custom_filters["user"] = filters.get('user')
+
+    list_of_payments = frappe.get_list("Allowed Payment Roles", 
+        filters = custom_filters,
+        fields = ['name','parent']
+    )
+
+    all_payment_modes = [[mode] for mode in set(map(lambda x: x.get('parent'),list_of_payments))]
+    return all_payment_modes
