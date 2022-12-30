2ee2badd2e68324c18dd7196e62562fed1e9c72c
kipngetich <kipngetich@finplusgroup.com>
2022-12-30 13:20:10 +0300

    allow permissions for payment modes child table

--- feeds/custom_methods/payment_entry.py
+++ feeds/custom_methods/payment_entry.py
@@ -13,7 +13,8 @@ def filter_payment_modes(doctype, txt, searchfield, start, page_len, filters):
 
     list_of_payments = frappe.get_list("Allowed Payment Roles", 
         filters = custom_filters,
-        fields = ['name','parent']
+        fields = ['name','parent'],
+        ignore_permissions=True
     )
 
     all_payment_modes = [[mode] for mode in set(map(lambda x: x.get('parent'),list_of_payments))]
