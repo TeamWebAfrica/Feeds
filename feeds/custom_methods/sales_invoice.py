fb5c3bb374652ccbfc160c0253ac67381759b230
kipngetich <kipngetich@finplusgroup.com>
2023-01-02 17:01:23 +0300

    Filter user income

--- feeds/custom_methods/sales_invoice.py
+++ feeds/custom_methods/sales_invoice.py
@@ -112,3 +112,32 @@ def get_user_defaults(user):
 		'default_warehouse':default_warehouse,
 		'default_income_account': default_income_account
 	}
+
+
+@frappe.whitelist(allow_guest=True)
+def filter_user_income_account(doctype, txt, searchfield, start, page_len, filters):
+    '''
+    Function that filters the different modes of payments for the curent user
+    '''
+    print("*"*80)
+	# get default income account
+    user = filters.get('user')
+    default_income_account = get_default_user_account(user)
+    if(default_income_account.get('status')):
+        print(default_income_account)
+
+    return []
+
+
+    # custom_filters = {}
+    # if filters.get('user') != 'Administrator':
+    #     custom_filters["user"] = filters.get('user')
+
+    # list_of_payments = frappe.get_list("Allowed Payment Roles", 
+    #     filters = custom_filters,
+    #     fields = ['name','parent'],
+    #     ignore_permissions=True
+    # )
+
+    # all_payment_modes = [[mode] for mode in set(map(lambda x: x.get('parent'),list_of_payments))]
+    # return all_payment_modes
