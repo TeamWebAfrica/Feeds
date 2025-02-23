b17f016dfb84beae638f92da95f99e1ce511f53b
Kipngtich33 <khalifngeno@gmail.com>
2025-02-23 12:09:26 +0100

    Set stock product bundle items as formula

--- feeds/custom_methods/item.py
+++ feeds/custom_methods/item.py
@@ -6,3 +6,6 @@ def before_save(doc,event):
     if doc.item_group == "Materials" or doc.item_group == "Additives":
         if not doc.is_stock_item:
             doc.is_stock_item = 1
+    elif self.item_group == "Formula":
+        if self.is_stock_item:
+            self.is_stock_item = 0
